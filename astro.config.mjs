95087eb1413b0eebb584926e1fe6c91244e55760
Junayd Bhoyroo <contact@junaydb.com>
2024-09-28 22:28:07 +0100

    fix: remove aws adapter
    Former-commit-id: ea9a2f124e485dec33b9830b1c7ab099defb2bd4
    Former-commit-id: 0c54ec70192f3b856f258636447ebef0cd748608 [formerly 438c6711c42950a71b18b5f5a6fe0999d483fe43] [formerly 5aaf89a7a8670f5a17006b909e7cecb2c9b64e86 [formerly e67dd2a4bfc5eb84370446160daf4069d1fb547a]]
    Former-commit-id: 6d76ec41a2681d9faafaac7b0e6d066db139cf63 [formerly d223430fe6a0ecf781e3fb8743a9aaf5b4519037]
    Former-commit-id: f8a223d4a8d07fe79e134483d97f0257bb8d3ef2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import aws from "astro-sst";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  adapter: aws(),
   prefetch: true,
   integrations: [mdx()],
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 });

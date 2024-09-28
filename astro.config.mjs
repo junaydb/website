84b325012a52d3f1e0fd20706a43b759113e232e
Junayd Bhoyroo <contact@junaydb.com>
2024-09-28 22:33:36 +0100

    fix: add missing package
    Former-commit-id: fa1b75b30559b3e3857d8fb1dff41e6a9a7ea473
    Former-commit-id: c19bad0cc478ccb4521ad5a93d1f2e3861272cf9 [formerly e3fabbc97162d438673eb0384b55181b1823ba3a] [formerly 6485ac768199c2dfbad6287b891e86d9dea3fc24 [formerly 90151a8d2c256d96f97806163d46418a83fca857]]
    Former-commit-id: eefc7625072d12eeec95c18b963b847549a88012 [formerly af457641abf76e07ca458f5adc0d461680ecef3b]
    Former-commit-id: 59ada8083b1b2c9f96b68a587f6c95d98f503a63

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({

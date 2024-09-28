2f9e47660dd3c8c2deeda57544a15c9be437dbf7
Junayd Bhoyroo <hello@junaydbhoyroo.com>
2024-09-28 18:04:26 +0100

    build: remove docker resources

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import aws from "astro-sst";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "hybrid",
+  adapter: aws(),
   prefetch: true,
   integrations: [mdx()],
 });

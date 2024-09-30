4f1bf6208f1ec3b70fde56adb602593532765514
Junayd Bhoyroo <contact@junaydb.com>
2024-09-30 22:49:16 +0100

    upgrade to Astro v5-beta
    Former-commit-id: 60c8fe815d3c601f3a045413b273ead94eebfbcb
    Former-commit-id: 6facf07a5c49a1eeefba5f3d586de60ef3c484c0 [formerly 12c9643fefb74857f79ed8b897c4d0c71412ae98] [formerly f1edd734990d072f2dff97baa43613bd6f30db30 [formerly 5dbc725fba4f2551bbdd371c99943212a7c2795f]]
    Former-commit-id: ec2bc65fc9530326a354f55281aa566361b926b6 [formerly 5260f10814be271428fe5ad64577ae33ccbdc27a]
    Former-commit-id: f8fab871999810be2cbd95eb308de94468744649

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import vercel from "@astrojs/vercel/serverless";
+// import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
+  // output: "hybrid",
   prefetch: true,
   integrations: [mdx()],
-  adapter: vercel({
-    webAnalytics: { enabled: true },
-  }),
+  // adapter: vercel({
+  //   webAnalytics: { enabled: true },
+  // }),
 });

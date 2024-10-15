bfbeee570fae634f835a96fd5714c220d502c756
Junayd Bhoyroo <contact@junaydb.com>
2024-10-15 21:14:21 +0100

    add maintenance mode and rename images to assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,7 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-// import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
 export default defineConfig({
-  // output: "hybrid",
   prefetch: true,
   integrations: [mdx()],
-  // adapter: vercel({
-  //   webAnalytics: { enabled: true },
-  // }),
 });
---
+++ middleware.js
@@ -0,0 +1,7 @@
+import { rewrite } from "@vercel/edge";
+
+export default function middleware(request) {
+  if (process.env.MAINTENANCE_MODE === "1") {
+    return rewrite(new URL("/maintenance", request.url));
+  }
+}

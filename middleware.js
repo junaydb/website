47fd8637a37ef3ba2a28cb1702ec2f9cfc1d7854
Junayd Bhoyroo <contact@junaydb.com>
2024-10-16 19:23:51 +0100

    bypass maintenance mode on staging deployment
    Former-commit-id: 2fc111e5f8fe25bd30127d03030ad3556f4613cd
    Former-commit-id: e9eb308b5cdb227de314a78ffd95aed584dc3cfd [formerly ef9870cb6e43dc36b09481aac59a8f5427ca53ce] [formerly df7a3835b3ab0402c1513f65b811a1ce7d10efcd [formerly 03500d8053c7ddf6616d3003986961505000fdae]]
    Former-commit-id: 7f8364c65323f9916140f9de06a9d6456c687cce [formerly d71db5d7e90f0068c2e0a7520abdc9e1dccf7359]
    Former-commit-id: ee74fe53f2a014f17964072390b218aad24e7252

--- middleware.js
+++ middleware.js
@@ -6,9 +6,10 @@ import { rewrite } from "@vercel/edge";
 import { get } from "@vercel/edge-config";
 
 export default async function middleware(request) {
-  const maintenanceMode = await get("maintenance");
-
-  if (maintenanceMode == "1") {
-    return rewrite(new URL("/maintenance", request.url));
+  if (!process.env.STAGING) {
+    const maintenanceMode = await get("maintenance");
+    if (maintenanceMode == "1") {
+      return rewrite(new URL("/maintenance", request.url));
+    }
   }
 }

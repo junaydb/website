0dbf3b1fbef9d42d4c0ce24870bac39ee4648472
Junayd Bhoyroo <contact@junaydb.com>
2024-11-13 22:01:02 +0000

    enable posts
    Former-commit-id: 17eb8e8edf13a6aa1840a403c8055703f0269940
    Former-commit-id: 2738f62d2676bfc1cda73fcf106529ee29767bb1 [formerly a06573791cbed720004991fcfb7368dda99915d1] [formerly d7b8c7e9b473844bf0734b76ce38d662c2ee5a4e [formerly 9e261f3715067a36ae3236ca5ee5fc266f3e617f]]
    Former-commit-id: dea474e53e6162fc5608d5b4cf651214951be452 [formerly 48c8aea52066752244868597a4bf8ca11beb7b72]
    Former-commit-id: 44aff198bfe6b1864a6b677df0cd854ccb457732

--- middleware.js
+++ middleware.js
@@ -1,9 +1,9 @@
+import { rewrite } from "@vercel/edge";
+import { get } from "@vercel/edge-config";
+
 export const config = {
   matcher: ["/", "/art/:path*", "/dev/:path*"],
 };
-
-import { rewrite } from "@vercel/edge";
-import { get } from "@vercel/edge-config";
 
 export default async function middleware(request) {
   if (process.env.PRODUCTION) {
